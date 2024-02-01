a304fda77884bf635b459cc19f66a4ff31435cbe
Hernan Amiune <amiune@gmail.com>
2024-02-01 22:13:32 +0100

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -2,21 +2,24 @@
 // https://github.com/markdown-it/markdown-it
 // https://stackoverflow.com/questions/69595539/how-to-get-some-markdown-from-an-external-resource-and-render-it-to-an-html-page
 
-blog_url = "https://www.generativenetworks.com/blog/";
-url = window.location.href;
-markdown_to_fetch = "";
 
-if(url.includes("#!")){
-    // show blog post
-    post_name = url.split("#!")[1];
-    markdown_to_fetch = blog_url + "posts/" + post_name + ".md";
-}
-else{
-    // show blog list of posts
-    markdown_to_fetch = blog_url + "postlist.md";
-}
 
 function reload_page(){
+    
+    blog_url = "https://www.generativenetworks.com/blog/";
+    url = window.location.href;
+    markdown_to_fetch = "";
+
+    if(url.includes("#!")){
+        // show blog post
+        post_name = url.split("#!")[1];
+        markdown_to_fetch = blog_url + "posts/" + post_name + ".md";
+    }
+    else{
+        // show blog list of posts
+        markdown_to_fetch = blog_url + "postlist.md";
+    }
+        
     var md = window.markdownit(); 
     fetch(markdown_to_fetch)
     .then((response) => response.text())
@@ -25,12 +28,9 @@ function reload_page(){
     })
 }
 
-window.addEventListener('locationchange', function () {
-    alert("locationchange");
-});
-
-window.addEventListener('popstate', function () {
-    alert("popstate");
+window.addEventListener('popstate', function (event) {
+    alert('popstate');
+    reload_page();
 });
 
 reload_page()
