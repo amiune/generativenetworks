27f42d08387b7c93f59885f633fe37f0e889c306
Hernan Amiune <amiune@gmail.com>
2024-02-03 13:34:19 +0100

    Refactored URLs

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,28 +1,33 @@
-// https://github.com/showdownjs/showdown
-// https://github.com/markdown-it/markdown-it
-// https://stackoverflow.com/questions/69595539/how-to-get-some-markdown-from-an-external-resource-and-render-it-to-an-html-page
+/*!
+ * Nano Blog
+ * Minimal javascript ajax blog for github pages
+ * using markdown for posts
+ *
+ * Developer: Hernan Amiune
+ * Copyright - Licensed under MIT
+ */
 
-function makeTitle(slug) {
+const BLOG_URL = "https://www.generativenetworks.com/blog/";
+const REPO_URL = "https://api.github.com/repos/amiune/generativenetworks/";
+
+function make_title(slug) {
     var words = slug.split('-');
-  
     for (var i = 0; i < words.length; i++) {
       var word = words[i];
       words[i] = word.charAt(0).toUpperCase() + word.slice(1);
     }
-  
     return words.join(' ');
   }
 
 function reload_page(){
     
-    blog_url = "https://www.generativenetworks.com/blog/";
     url = window.location.href;
     markdown_to_fetch = "";
 
     if(url.includes("#!")){
         // show blog post
         post_name = url.split("#!")[1];
-        markdown_to_fetch = blog_url + "posts/" + post_name + ".md";
+        markdown_to_fetch = BLOG_URL + "posts/" + post_name + ".md";
 
         var md = window.markdownit(); 
         fetch(markdown_to_fetch)
@@ -33,29 +38,30 @@ function reload_page(){
     }
     else{
         // show blog list of posts
-        url = "https://api.github.com/repos/amiune/generativenetworks/git/trees/main?recursive=1"
+        url = REPO_URL + "git/trees/main?recursive=1";
         fetch(url)
             .then((response) => response.text())
             .then((text) => {
                 const r = JSON.parse(text);
                 files_list = r.tree;
                 let list = document.createElement("ul");
-                for (let i = 0; i < files_list.length; i++) {
+                // show posts from oldest to newest
+                for (let i = files_list.length - 1; i >= 0; i--) {
                     const element = files_list[i];
                     if(element.path.includes("posts/")){
                         let li = document.createElement('li');
                         let file_name = element.path.split("posts/")[1];
                         let post_name = file_name.replace(".md","");
                         let a = document.createElement('a');
-                        a.innerText = makeTitle(post_name);
-                        a.setAttribute("href", "https://www.generativenetworks.com/blog/blog.html#!" + post_name)
+                        a.innerText = make_title(post_name);
+                        a.setAttribute("href", BLOG_URL + "index.html#!" + post_name)
                         li.appendChild(a);
                         list.appendChild(li);
                     }
                 }
                 let div = document.createElement('div');
                 let h1 = document.createElement('h1');
-                h1.innerText = "Generative Networks Blog";
+                h1.innerText = document.title;
                 div.appendChild(h1);
                 div.appendChild(list);
                 document.getElementById('content').innerHTML = div.innerHTML;
